359b2ca135084c4bc30701ca861d05631801b29c
Luarewik <49365182+Luarewik@users.noreply.github.com>
2021-10-20 22:51:40 +0200

    Adding user table model

---
+++ src/main/java/com/example/alertme/api/models/User.java
@@ -0,0 +1,107 @@
+package com.example.alertme.api.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+public class User {
+    private @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+
+    private String first_name;
+    private String last_name;
+    private String login;
+    private String password_hash;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String first_name, String last_name, String login, String password_hash, String email) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.login = login;
+        this.password_hash = password_hash;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(first_name, user.first_name) &&
+                Objects.equals(last_name, user.last_name) &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(password_hash, user.password_hash) &&
+                Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, first_name, last_name, login, password_hash, email);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword_hash() {
+        return password_hash;
+    }
+
+    public void setPassword_hash(String password_hash) {
+        this.password_hash = password_hash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", login='" + login + '\'' +
+                ", password_hash='" + password_hash + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
