c4d17249c9e30168266de368c04547df369e0917
Luarewik <49365182+Luarewik@users.noreply.github.com>
2021-10-20 21:52:06 +0200

    Initial commit

---
+++ src/main/java/com/example/alertme/AlertMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.alertme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlertMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alertme/AlertMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alertme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
