79a5fecab1b29178758efd353db6edc14e257e5f
Luarewik <49365182+Luarewik@users.noreply.github.com>
2021-10-20 22:33:21 +0200

    Adding Password encoder and cors mapping

--- src/main/java/com/example/alertme/AlertMeApplication.java
+++ src/main/java/com/example/alertme/AlertMeApplication.java
@@ -2,12 +2,34 @@ package com.example.alertme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AlertMeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlertMeApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("PUT", "POST", "GET", "DELETE");
+            }
+        };
     }
 
 }
