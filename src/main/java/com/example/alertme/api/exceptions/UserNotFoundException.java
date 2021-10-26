d3654b25b021c385b6269dcb482dd0bfb2a266dc
Luarewik <49365182+Luarewik@users.noreply.github.com>
2021-10-27 00:45:02 +0200

    Dodanie parametru errorCode

--- src/main/java/com/example/alertme/api/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/alertme/api/exceptions/UserNotFoundException.java
@@ -1,7 +1,14 @@
 package com.example.alertme.api.exceptions;
 
 public class UserNotFoundException extends RuntimeException  {
+
+    private final int errorCode = 11;
+
     public UserNotFoundException(String text) {
         super("Could not find user: " + text);
+    }
+
+    public int getErrorCode() {
+        return errorCode;
     }
 }
