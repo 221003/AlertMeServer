09f5d3c0d5b9d0cce995eb8421f6bfb505931893
Luarewik <49365182+Luarewik@users.noreply.github.com>
2021-11-04 22:05:27 +0100

    Dodanie zmiany hasła, poprawienie dodawania nowego użytkownika i dodanie pobierania alertów użytkownika.

--- src/main/java/com/example/alertme/api/repositories/AlertRepository.java
+++ src/main/java/com/example/alertme/api/repositories/AlertRepository.java
@@ -1,7 +1,14 @@
 package com.example.alertme.api.repositories;
 
 import com.example.alertme.api.models.Alert;
+import com.example.alertme.api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
 public interface AlertRepository extends JpaRepository<Alert, Long> {
+    List<Alert> findByUser_id(Long user_id);
 }
