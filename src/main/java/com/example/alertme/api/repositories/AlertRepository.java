417a8f138d42e86fb1948d2f9ec18aaff7a38a5c
majkelw <michalwik@vp.pl>
2021-11-16 22:05:24 +0100

    Added repository for Vote + transactional annotation

--- src/main/java/com/example/alertme/api/repositories/AlertRepository.java
+++ src/main/java/com/example/alertme/api/repositories/AlertRepository.java
@@ -2,6 +2,8 @@ package com.example.alertme.api.repositories;
 
 import com.example.alertme.api.models.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface AlertRepository extends JpaRepository<Alert, Long> {
 }
--- src/main/java/com/example/alertme/api/repositories/VoteRepository.java
+++ src/main/java/com/example/alertme/api/repositories/VoteRepository.java
@@ -1,11 +1,13 @@
 package com.example.alertme.api.repositories;
 
-import com.example.alertme.api.models.User;
+import com.example.alertme.api.models.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 @Transactional
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findOneByLoginOrEmail(String login, String email);
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    List<Vote> findByAlertIdAndUserId(Long alertId, Long userId);
+
 }
