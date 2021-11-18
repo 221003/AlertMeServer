97ae60e134edf9a57a97757dd5eba402795286cd
Luarewik <221003@edu.p.lodz.pl>
2021-11-18 18:43:49 +0100

    Add scheduled task of sending notifications

---
+++ src/main/java/com/example/alertme/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.alertme.config;
+
+import com.example.alertme.api.repositories.UserRepository;
+import com.example.alertme.services.NotificationTask;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class AppConfig {
+
+}
